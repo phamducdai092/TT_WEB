dbde79f9edc61f2b5c01c842c76cab2dcd64e304
VoPhong2503 <voquocphong6678@gmail.com>
2024-07-17 17:21:01 +0700

    ghi log khi admin thuc hien an hoac hien brand

--- src/main/java/controller/admin/AdminHiddenBrand.java
+++ src/main/java/controller/admin/AdminHiddenBrand.java
@@ -1,12 +1,17 @@
 package controller.admin;
 
+import bean.Brand;
+import bean.Log;
+import bean.User;
 import dao.BrandDAO;
+import service.LogService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(value = "/hiddenBrand")
@@ -18,8 +23,34 @@ public class AdminHiddenBrand extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User u = (User) session.getAttribute("auth");
+
+        String preData, afterData, table, role, actionDetail;
         int brandId = Integer.parseInt(req.getParameter("brandId"));
-        BrandDAO.hiddenBrand(brandId);
-        req.getRequestDispatcher("./adminBrandManagement").forward(req, resp);
+        Brand brand = BrandDAO.getBrandById(brandId);
+
+        LogService usl= new LogService();
+        Log us=new Log();
+
+        int userId = u.getId();
+
+
+        table = "brands";
+        us.setTables(table);
+
+        role = "adminId = " + userId;
+        us.setRole(role);
+
+        actionDetail = "disable";
+        us.setActionDetail(actionDetail);
+
+        if (brandId != 0) {
+            BrandDAO.hiddenBrand(brandId);
+            req.getRequestDispatcher("./adminBrandManagement").forward(req, resp);
+            preData= "disatble brandId = " + brandId;
+            us.setBeforeData(preData);
+            usl.update(us);
+        }
     }
 }
