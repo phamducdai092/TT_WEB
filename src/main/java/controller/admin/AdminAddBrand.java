7bb4ba223de26f0605695f8d1d5f63d906b5d976
VoPhong2503 <voquocphong6678@gmail.com>
2024-07-17 17:18:20 +0700

    ghi log khi admin them brand moi

--- src/main/java/controller/admin/AdminAddBrand.java
+++ src/main/java/controller/admin/AdminAddBrand.java
@@ -1,12 +1,17 @@
 package controller.admin;
 
+import bean.Brand;
+import bean.Log;
+import bean.User;
 import dao.BrandDAO;
+import service.LogService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(value = "/addBrand")
@@ -20,11 +25,34 @@ public class AdminAddBrand extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User u = (User) session.getAttribute("auth");
+
         newBrand = req.getParameter("new-brand");
         boolean addSuccess = BrandDAO.addNewBrand(newBrand);
+        Brand brand = BrandDAO.getBrandByName(newBrand);
 
+        String preData, afterData, table, role, actionDetail;
+
+        LogService usl= new LogService();
+        Log us=new Log();
+
+        int userId = u.getId();
+
+
+        table = "brands";
+        us.setTables(table);
+
+        role = "adminId = " + userId;
+        us.setRole(role);
+
+        actionDetail = "addBrand";
+        us.setActionDetail(actionDetail);
         if(addSuccess) {
             req.getRequestDispatcher("./adminBrandManagement").forward(req, resp);
+            afterData = brand.getId() + ", " + newBrand;
+            us.setAfterData(afterData);
+            usl.insert(us);
         }else {
             req.setAttribute("error", error);
             req.getRequestDispatcher("./adminBrandManagement").forward(req, resp);
